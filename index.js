571ca2ab71e6d25fa38f83f719babc974824cfaa
Ezekiel Fallarcuna <105767179+acefallarcuna@users.noreply.github.com>
2024-08-23 20:18:50 +0800

    added setInt

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const Discord = require('discord.js'); //import client from discord
-const keep_alive = require('./keep_alive.js')
+const Discord = require('discord.js'); // Import client from discord
+const keep_alive = require('./keep_alive.js');
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  // Send a message
+  setInterval(() => {
+    const channel = client.channels.cache.get('1276515657558654986'); // Replace with your channel ID
+    if (channel) {
+      channel.send('This is a message sent every 60 seconds!');
+    }
+  }, 120000);
 });
 
 client.on('message', msg => {
-    // check if message isn't from us
-    if (msg.author == client.user) {
-      return;
-    }
-    else if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-    else {
-        msg.reply(msg.content);
-        console.log(msg.content);
-    }
+  // Check if message isn't from the bot itself
+  if (msg.author == client.user) {
+    return;
+  } else if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  } else {
+    msg.reply(msg.content);
+    console.log(msg.content);
+  }
 });
 
-client.login(process.env.TOKEN); //login bot using token
+client.login(process.env.TOKEN); // Login bot using token
